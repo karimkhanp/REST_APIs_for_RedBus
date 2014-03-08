35e256ff549a530d2ad808e3b34661073314d88f
Karimkhan <karimkhan_it@yahoo.com>
2014-03-07 19:41:43 -0800

    Added few more APIs call like getting dropping, borading point etc.
    Need to be polished

--- main.php
+++ main.php
@@ -7,6 +7,54 @@
 			getbranch($getbranch);
 		}
 	}
+
+	if(isset($_GET['getcity']))
+	{
+		$getcity = $_GET['getcity'];
+		if($getcity == 'true')
+		{
+			getcity($getcity);
+		}
+	}
+
+	if(isset($_GET['getsourcetodest']))
+	{
+		$getsourcetodest = $_GET['getsourcetodest'];
+		if($getsourcetodest == 'true')
+		{
+			getsourcetodest($getsourcetodest);
+		}
+	}
+
+	if(isset($_GET['getAvailRout']))
+	{
+		$getAvailRout = $_GET['getAvailRout'];
+		if($getAvailRout == 'true')
+		{
+			getAvailRout($getAvailRout);
+		}
+	}
+
+	if(isset($_GET['getBoardingPoint']))
+	{
+		$getBoardingPoint = $_GET['getBoardingPoint'];
+		$city = $_GET['city'];
+		if($getBoardingPoint == 'true')
+		{
+			getBoardingPoint($getBoardingPoint,$city);
+		}
+	}
+
+	if(isset($_GET['getDropingPoint']))
+	{
+		$getDropingPoint = $_GET['getDropingPoint'];
+		$city = $_GET['city'];
+		if($getDropingPoint == 'true')
+		{
+			getDropingPoint($getDropingPoint,$city);
+		}
+	}
+
 	function getbranch($getbranch)
 	{
 		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');
@@ -17,12 +65,109 @@
 		}   
 		$today = date("Ymd");           
 		$result1 = array();
-		$result = mysqli_query($con,"SELECT sno,division, branch,branchofficename,branchofficecode,status from tbl_branceoffice");
+		$result = mysqli_query($con,"SELECT sno,division, branch,branchofficename,branchofficecode,status from tbl_demo");
 		while ($row = @mysqli_fetch_array($result))
 		{
 			array_push($result1,$row);
 		}
-		echo  $result1 = json_encode($result1);  
+		echo $result1 = json_encode($result1);  
+		
+	}
+
+	function getcity($getcity)
+	{
+		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');
+		if (mysqli_connect_errno())
+		{
+		    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		    return;
+		}   
+		$today = date("Ymd");           
+		$result1 = array();
+		$result = mysqli_query($con,"SELECT sno,city,status from tbl_demo");
+		while ($row = @mysqli_fetch_array($result))
+		{
+			array_push($result1,$row);
+		}
+		echo  $result1 = json_encode($result1,true);  
+	}
+
+	function getsourcetodest($getsourcetodest)
+	{
+
+		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');
+		if (mysqli_connect_errno())
+		{
+		    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		    return;
+		}   
+		$today = date("Ymd");           
+		$result1 = array();
+		$result = mysqli_query($con,"SELECT * from tbl_newbusroute");
+		while ($row = @mysqli_fetch_array($result))
+		{	
+			array_push($result1,$row);
+		}
+		echo  $result1 = json_encode($result1,true);  
+	}
+
+	function getAvailRout($getAvailRout)
+	{
+
+		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');
+		if (mysqli_connect_errno())
+		{
+		    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		    return;
+		}   
+		$today = date("Ymd");           
+		$result1 = array();
+		$result = mysqli_query($con,"SELECT sno,source,routename from tbl_newbusroute where status = 'active'");
+		while ($row = @mysqli_fetch_array($result))
+		{	
+			array_push($result1,$row);
+		}
+		echo  $result1 = json_encode($result1,true);  
+	}
+
+	function getBoardingPoint($getBoardingPoint,$city)
+	{
+
+		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');
+		if (mysqli_connect_errno())
+		{
+		    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		    return;
+		}   
+		$today = date("Ymd");           
+		$result1 = array();
+		//echo $city;
+		$result = mysqli_query($con,"SELECT * from tbl_service_boarding where station_name = '".$city."'");
+		while ($row = @mysqli_fetch_array($result))
+		{	
+			array_push($result1,$row);
+			//echo "hi";
+		}
+		echo  $result1 = json_encode($result1,true);  
+	}
+
+	function getDropingPoint($getDropingPoint,$city)
+	{
+
+		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');
+		if (mysqli_connect_errno())
+		{
+		    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		    return;
+		}   
+		$today = date("Ymd");           
+		$result1 = array();
+		$result = mysqli_query($con,"SELECT * from tbl_service_droping where station_name = '".$city."'");
+		while ($row = @mysqli_fetch_array($result))
+		{	
+			array_push($result1,$row);
+		}
+		echo  $result1 = json_encode($result1,true);  
 	}
 	//echo $_GET['location'];
 	
