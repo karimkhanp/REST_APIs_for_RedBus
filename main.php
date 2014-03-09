524df3ade6035c0c627a6d2c82a19d3fbbd1dcd3
Karimkhan <karimkhan_it@yahoo.com>
2014-03-08 23:49:51 -0800

    Book ticket, cancel ticket function added

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
+	//echo date("ymd");
 	if(isset($_GET['getbranch']))
 	{
 		$getbranch = $_GET['getbranch'];
@@ -55,6 +56,16 @@
 		}
 	}
 
+	if(isset($_GET['doconfirm']))
+	{
+		$doConfirm = $_GET['doconfirm'];
+		$user_id = $_GET['user_id'];
+		if($doConfirm == 'true')
+		{
+			doConfirm($user_id);
+		}
+	}
+
 	if(isset($_GET['book_ticket']))
 	{
 		$book_ticket = mysql_real_escape_string($_GET['book_ticket']);
@@ -76,6 +87,150 @@
 
 	}
 
+	if(isset($_GET['isticketcancellable']))
+	{
+		$isticketcancellable = $_GET['isticketcancellable'];
+		$user_id = $_GET['user_id'];
+		$user_pnr = $_GET['user_pnr'];
+		if($isticketcancellable == 'true')
+		{
+			isTicketCancellable($user_id,$user_pnr);
+		}
+	}
+
+	if(isset($_GET['cancelticket']))
+	{
+		$cancelticket = $_GET['cancelticket'];
+		$user_id = $_GET['user_id'];
+		$user_pnr = $_GET['user_pnr'];
+		if($cancelticket == 'true')
+		{
+			cancelTicket($user_id,$user_pnr);
+		}
+	}
+
+	function cancelTicket($user_id,$user_pnr)
+	{
+
+		$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');				
+		if (mysqli_connect_errno())
+		{
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			return;
+		}		
+		$result = mysqli_query($con,"select date from tbl_user where user_id = '".$user_id."' AND user_pnr = '".$user_pnr."'");
+		$row = @mysqli_fetch_row($result);
+		$book_date = $row[0];
+		$today = date("ymd");
+		$diff = $today - $book_date;
+		//echo "<br>dif : $diff";
+		if($diff <= 1)
+		{
+			echo "Sorry, now you can not cancel your ticket ...";
+			return;
+		}
+		else
+		{
+			$result = mysqli_query($con,"select amount from tbl_confirm where user_id = '".$user_id."' AND user_pnr = '".$user_pnr."'");
+			$row = @mysqli_fetch_row($result);
+			$amount = $row[0];
+			echo $amount;
+			$updateStatus = "update tbl_user set isConfirm = 'cancelled' where user_id = '".$user_id."' AND user_pnr = '".$user_pnr."'";
+			if (!mysqli_query($con,$updateStatus))
+		  	{
+		  		//	die('Error: ' . mysqli_error($con));
+					echo "error";
+			}
+			$updateStatus = "update tbl_confirmed set status = 'cancelled' where user_id = '".$user_id."' AND user_pnr = '".$user_pnr."'";
+			if (!mysqli_query($con,$updateStatus))
+		  	{
+		  		//	die('Error: ' . mysqli_error($con));
+					echo "error";
+			}
+			echo "Your ticket with pnr number $user_pnr is cancelled ...<br>";
+			echo "Your accoutn would be credited by $amount by 7 working days ...<br>";
+			echo "Thanks for using our system!";
+
+		}
+
+	}
+
+
+	function isTicketCancellable($user_id,$user_pnr)
+	{
+			$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');				
+			if (mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				return;
+			}		
+			$result = mysqli_query($con,"select date from tbl_user where user_id = '".$user_id."' AND user_pnr = '".$user_pnr."'");
+			$row = @mysqli_fetch_row($result);
+			$book_date = $row[0];
+			echo $book_date;
+			$today = date("ymd");
+			$diff = $today - $book_date;
+			//echo "<br>dif : $diff";
+			if($diff <= 1)
+				echo "Sorry, could not be cancelled ..";
+			else
+				echo "You can proceed to cancel ..";
+	}
+
+	function doConfirm($user_id)
+	{
+			$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');				
+			if (mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				return;
+			}
+			$result = mysqli_query($con,"select * from tbl_user where user_id = '".$user_id."' AND isConfirm = 'No' ");
+			if(mysqli_num_rows($result)>0)
+			{			
+				while ($row = @mysqli_fetch_array($result))
+				{
+					confirmTicket($row['user_id'],$row['user_pnr'],$row['from'],$row['to'],$row['way'],$row['date'],$row['seat'],$row['email']);
+				}
+			}
+			else
+				echo "your all tickes are confirmed";
+			//$row = @mysqli_fetch_row($result);
+			//echo $row[0];
+			return;
+	}
+
+	function confirmTicket($user_id,$user_pnr,$from,$to,$way,$date,$seat,$email)
+	{
+		//Here assign the seats, after/before getting payment
+		//Calculate the ammount as per seat type
+		$amount = $seat * 200;
+		//Select seats from list of vacant seatas
+		$seat_no = '4,5';
+			$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');				
+			if (mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				return;
+			}
+			$insertQuery1 = "INSERT INTO tbl_confirmed(`user_id`,`from`,`to`,`date`,`user_pnr`,`seat`,`email`,amount) VALUES ('".$user_id."','".$from."','".$to."','".$date."','".$user_pnr."','".$seat_no."','".$email."','".$amount."')";
+			if (!mysqli_query($con,$insertQuery1))
+		  	{
+		  		//	die('Error: ' . mysqli_error($con));
+					echo "error";
+			}		
+			$updateStatus = "update tbl_user set isConfirm = 'Yes' where user_id = '".$user_id."' AND user_pnr = '".$user_pnr."'";
+			if (!mysqli_query($con,$updateStatus))
+		  	{
+		  		//	die('Error: ' . mysqli_error($con));
+					echo "error";
+			}		
+			echo "Your ticket is confirmed... <br>";
+			echo "you PNR is $user_pnr and seat no $seat_no <br>";
+			
+			return;		
+	}
+
 	function DoTentativeBooking($userid,$book_ticket,$from,$to,$date,$pnr,$seats,$email)
 	{
 			$con = mysqli_connect('127.0.0.1', 'root', '', 'safari');				
@@ -84,7 +239,7 @@
 				echo "Failed to connect to MySQL: " . mysqli_connect_error();
 				return;
 			}
-			$insertQuery1 = "INSERT INTO tbl_user(`user_id`,`book_ticket`,`from`,`to`,`date`,`user_pnr`,`seat`,`email`) VALUES ('".$userid."','".$book_ticket."','".$from."','".$to."','".$date."','".$pnr."','".$seats."','".$email."')";
+			$insertQuery1 = "INSERT INTO tbl_user(`user_id`,`book_ticket`,`from`,`to`,`date`,`user_pnr`,`seat`,`email`,`isConfirm`) VALUES ('".$userid."','".$book_ticket."','".$from."','".$to."','".$date."','".$pnr."','".$seats."','".$email."','No')";
 			if (!mysqli_query($con,$insertQuery1))
 		  		{
 		  		//	die('Error: ' . mysqli_error($con));
